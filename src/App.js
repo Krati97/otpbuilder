655807f9e315c49d3993be7fc402b9603789c269
Krati97 <kabara.krati@gmail.com>
2025-04-03 09:27:08 -0700

    Explaining comments

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import "./styles.css";
 
+// If number is changed below, the number of  otp digits will change automatically
 const Num_of_otp_digits = 5;
 export default function App() {
   const [inputArr, setInputArr] = useState(
@@ -9,21 +10,26 @@ export default function App() {
 
   const refArr = useRef([]);
 
+  // Focus on the first input when the component is mounted (for ex: on page refresh)
   useEffect(() => {
     refArr.current[0]?.focus();
   }, []);
 
   const handleOnChange = (value, index) => {
     if (isNaN(value)) return;
+
+    // Remove all spaces from the input value
     const newValue = value.trim();
     const newArr = [...inputArr];
     newArr[index] = newValue.slice(-1);
     setInputArr(newArr);
 
+    // Focus on the next input box if the current input is not empty
     newValue && refArr.current[index + 1]?.focus();
   };
 
   const handleOnKeyDown = (e, index) => {
+    // If the current input is empty and the backspace key is pressed, focus on the previous input box
     if (!e.target.value && e.key === "Backspace") {
       refArr.current[index - 1]?.focus();
     }
